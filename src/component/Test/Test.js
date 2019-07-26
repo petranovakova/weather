2cf7d76b7d47a2c091ceac25e24e44ba1fc9a7f2
pavelhruby <pavel.hruby@directfinance.cz>
2019-07-26 13:28:48 +0200

    navod na form submit

--- src/component/Test/Test.js
+++ src/component/Test/Test.js
@@ -3,48 +3,61 @@ import axios from 'axios'
 
 
 const cityList = require('./city.list.json');
-
-const niceList = cityList.map(city => ( {name: city.name, id: city.id}))
-
 const finalList = {};
-niceList.forEach(item => finalList[item.name] = item.id);
-
 
 class Test extends Component {
+    static apiKey = '94bfa8b7d67109e191ec4aef03ce391e';
+
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             city: '',
-        }
+        };
         this.findCity = this.findCity.bind(this);
-        this.findCityId = this.findCityId.bind(this);
+        this.findCitySubmit = this.findCitySubmit.bind(this);
+        // this.finalList = {};
+        // this.findCityId = this.findCityId.bind(this);
+        // this.polyfill();
+    }
+
+    findCitySubmit (event) {
+        console.log('submitting...');
+        console.log(this.state.city);
+        event.preventDefault();
     }
 
     findCity (event) {
+        // event.preventDefault();
         this.setState({city: event.target.value})
     }
-    findCityId ({city}) {
-        return finalList.city;
-    }
+    // findCityId ({city}) {
+    //     return finalList.city;
+    // }
 
     render() {
-        console.log(this.state.city)
+        console.log('finalList', finalList);
+        console.log("rendering...");
         return (
-            <form>
+            <div>
+            <form onSubmit={this.findCitySubmit}>
                 <input
                     type="text"
                     onChange={this.findCity}
+                    // value={this.state.city}
                 />
+                <button type="submit">ok</button>
             </form>
+                <div className="result">
+                    results....
+                </div>
+            </div>
         );
     }
 
-    static apiKey = '94bfa8b7d67109e191ec4aef03ce391e';
-
     componentDidMount() {
-        console.log('niceList', niceList);
-        // axios.get('https://openweathermap.org/data/2.5/forecast?id=524901&appid=94bfa8b7d67109e191ec4aef03ce391e')
-      console.log(axios.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=' + Test.apiKey));
+        cityList.forEach(city => finalList[city.name] = city.id);
+        console.log(finalList);
+        // console.log(axios.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=' + Test.apiKey));
     }
 
 }
