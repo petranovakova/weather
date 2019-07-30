eb7d72a46076982894f7671e1d333788cfbec9ae
pavelhruby <pavel.hruby@directfinance.cz>
2019-07-30 17:36:30 +0200

    cool stateless functional components

--- src/component/WeatherDataDisplay/WeatherDataDisplay.js
+++ src/component/WeatherDataDisplay/WeatherDataDisplay.js
@@ -1,20 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-class WeatherDataDisplay extends Component {
-    render() {
-
-        const {data} = this.props;  // data neni array, ale object, takze nemuzes volat .map()
-        console.log(data);
-
-        // zobrazim data jen, pokud data neni null
-        return data && <div>
-            <ul>
-                <li>id: {data.id}</li>
-                <li>name: {data.name}</li>
-                <li>visibility: {data.visibility}</li>
-            </ul>
-        </div>;
-    }
-}
+const WeatherDataDisplay = ({data}) => data && <div>
+    <ul>
+        <li>id: {data.id}</li>
+        <li>name: {data.name}</li>
+        <li>visibility: {data.visibility}</li>
+    </ul>
+</div>;
 
 export default WeatherDataDisplay;
