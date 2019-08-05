b09cb337994adac75708c2d9cc95bc206877642e
pavelhruby <pavel.hruby@directfinance.cz>
2019-08-05 17:09:36 +0200

    navrh hodin

--- src/component/WeatherDataDisplay/WeatherDataDisplay.js
+++ src/component/WeatherDataDisplay/WeatherDataDisplay.js
@@ -13,9 +13,24 @@ const WeatherDataDisplay = ({data, city}) => {
         return value.dt_txt.slice(0, 11);
     });
 
-    return groupedByDayData.map(day => {
-        return <DailyData data={day} key={day.key} />
-    });
+    console.log(groupedByDayData);
+
+    return <div>
+        // hours
+        <div>00:00</div>
+        <div>03:00</div>
+        <div>06:00</div>
+        <div>09:00</div>
+        <div>12:00</div>
+        <div>15:00</div>
+        <div>18:00</div>
+        <div>21:00</div>
+
+        //data
+        {groupedByDayData.map(day => {
+            return <DailyData data={day} key={day.key}/>
+        })}
+    </div>;
 };
 
 export default WeatherDataDisplay;
